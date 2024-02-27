626f1bf2bdcafa4b3d773b4772796f906900273e
amirali115a <127670980+amirali115a@users.noreply.github.com>
2024-02-27 20:09:24 +0330

    xss force file

---
+++ lib/helper/Log.py
@@ -0,0 +1,17 @@
+
+from lib.helper.helper import * 
+from datetime import datetime
+class Log:
+
+	@classmethod
+	def info(self,text):
+ 		print("["+Y+datetime.now().strftime("%H:%M:%S")+N+"] ["+G+"INFO"+N+"] "+text)
+ 
+	@classmethod
+	def warning(self,text):
+		print("["+Y+datetime.now().strftime("%H:%M:%S")+N+"] ["+Y+"WARNING"+N+"] "+text)
+
+	@classmethod
+	def high(self,text):
+ 		print("["+Y+datetime.now().strftime("%H:%M:%S")+N+"] ["+R+"CRITICAL"+N+"] "+text)
+ 		
